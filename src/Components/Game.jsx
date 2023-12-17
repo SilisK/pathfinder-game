f9f55bd8e70da53298a519f1a5618c17ed704ae8
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-12-17 13:17:29 -0500

    implemented gameplay stemming from play.jsx

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,36 +1,16 @@
-import { useState } from "react";
-import SeedPreview from "./SeedPreview";
 import Gameplay from "./Gameplay";
 
-export default function Game() {
-  const [gameplayActive, setGameplay] = useState(false);
-  const [seed, setSeed] = useState();
-
+export default function Game({ seed }) {
   return (
-    <div className="game grid place-items-center w-full min-h-full">
-      <div className="modal sm:border rounded px-3 pt-3 pb-1 w-full">
-        {gameplayActive ? (
-          <Gameplay
-            gameInfo={{
-              ...seed,
-              maxChoices: 3,
-            }}
-          />
-        ) : (
-          <SeedPreview
-            image={
-              "https://pathfinder-prototype.netlify.app/assets/on-galaxys-edge-variant-4-61f553b7.png"
-            }
-            title={"On Galaxy's Edge"}
-            plot={
-              "You are an intergalactic space traveler who has discovered teleportation technology."
-            }
-            setGameplay={setGameplay}
-            setSeed={setSeed}
-          />
-        )}
+    <div className="z-10 game grid place-items-center w-full min-h-full">
+      <div className="modal sm:border rounded px-3 pt-3 pb-1 w-max">
+        <Gameplay
+          gameInfo={{
+            ...seed,
+            maxChoices: 3,
+          }}
+        />
       </div>
     </div>
   );
 }
-
