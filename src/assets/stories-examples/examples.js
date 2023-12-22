642465831ff93469a57c3657db17eb594af1daf5
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-12-22 01:15:49 -0500

    style:updated responsiveness

---
+++ src/assets/stories-examples/examples.js
@@ -0,0 +1,23 @@
+import great_escape_img from "../image-examples/the-great-escape-variant-2.png";
+import galaxy_edge_img from "../image-examples/on-galaxys-edge-variant-4.png";
+import love_life_img from "../image-examples/the-love-of-my-life-variant-2.png";
+
+const adventures = [
+  {
+    image: great_escape_img,
+    title: "The Great Escape",
+    plot: "You are a wrongfully convicted murderer on death row, awaiting your sentence.",
+  },
+  {
+    image: galaxy_edge_img,
+    title: "On Galaxy's Edge",
+    plot: "You are an intergalactic space traveler who has discovered teleportation technology.",
+  },
+  {
+    image: love_life_img,
+    title: "The Love of My Life",
+    plot: "You have fallen in love with the most wonderful person but there is more to them than meets the eye.",
+  },
+];
+
+export default adventures;
