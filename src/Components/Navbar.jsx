aa81fe4ed699cfc046a45296ab5e68afa42461bd
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-12-21 22:58:37 -0500

    style:made homepage and about page responsive, optimizing:made arrays to map repetitive data

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,39 @@
 import { Link } from "react-router-dom";
+import hamburger_menu from "../assets/gizmos/hamburger-menu.png";
+import close_button from "../assets/gizmos/close.png";
+import { useEffect, useState } from "react";
 
 function NavBar() {
-  return (
-    <nav className="w-full flex items-center justify-between p-5 text-white position: fixed z-50">
-      <Link to="/" className="text-xl flex">
-        <div className="logo"></div>
-        <div className="mt-1">
-          <h1 className="uppercase leading-loose tracking-wider font-bold">Pathfinder</h1>
-        </div>
-      </Link>
+  const [navToggled, setNavToggled] = useState(false);
 
-      <div className="w-3/12 flex justify-between">
+  return (
+    <nav className="w-screen bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-md flex flex-col items-center justify-between p-2 text-white fixed z-50 md:flex-row">
+      {/* Logo */}
+      <div className="flex items-center justify-between w-full">
+        <Link to="/" className="flex items-center sm:text-xl">
+          <div className="logo"></div>
+          <h1 className="uppercase leading-loose tracking-wider font-bold">
+            Pathfinder
+          </h1>
+        </Link>
+        <div
+          className="cursor-pointer md:hidden"
+          onClick={() => setNavToggled(!navToggled)}
+        >
+          <img
+            src={navToggled ? close_button : hamburger_menu}
+            className="select-none pointer-events-none w-10"
+          />
+        </div>
+      </div>
+      {/* Nav Links */}
+      <div
+        className={`w-full ${
+          navToggled
+            ? "py-5 flex flex-col items-center"
+            : "hidden"
+        } justify-between md:flex md:bg-transparent md:flex-row md:w-max`}
+      >
         <Link to="/" className="text-md px-6 py-2 hover-underline">
           Home
         </Link>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,61 +1,84 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from "react";
+import github_icon from "../assets/gizmos/github-icon.png";
+import linkedin_icon from "../assets/gizmos/linkedin-icon.png";
 
+const team_members = [
+  {
+    name: "Berlin Rivas",
+    avatarClassName: "berlin-avatar",
+    github: "https://github.com/BerlinRivas",
+    linkedin: "",
+  },
+  {
+    name: "Frantz-Sebastien Mathias",
+    avatarClassName: "frantz-avatar",
+    github: "https://github.com/Frantz-Sebastien",
+    linkedin: "https://www.linkedin.com/in/fsmathias/",
+  },
+  {
+    name: "Wai Leong Chong",
+    avatarClassName: "wai-avatar",
+    github: "https://github.com/waiLeongChong",
+    linkedin: "https://www.linkedin.com/in/wai-leong-chong/",
+  },
+  {
+    name: "Silis Kleemoff",
+    avatarClassName: "silis-avatar",
+    github: "https://github.com/KleemoffDeveloper",
+    linkedin: "https://www.linkedin.com/in/silis-kleemoff/",
+  },
+];
 
 function About() {
-
   useEffect(() => {
     waves();
   }, []);
-
-
   return (
-    <div className="text-white max-w-7xl m-auto h-screen flex flex-col items-center justify-center">
-
-      <div className="w-3/5 mb-24 z-40">
-        <h2 className="text-4xl mb-4 tracking-wider">What is <span className="text-blue-300">PATHFINDER</span> Exactly</h2>
+    <div className="text-white max-w-7xl m-auto py-24 min-h-screen flex flex-col items-center justify-center">
+      <div className="w-11/12 mb-24 z-10 md:w-3/5">
+        <h2 className="text-4xl mb-4 tracking-wider">
+          What is <span className="text-blue-300">PATHFINDER</span> Exactly
+        </h2>
         <p className="leading-relaxed text-base">
-          PathFinder is a gaming web app that uses ChatGPT and Dall-E to create dynamic story-telling experiences.
-          <br></br><br></br>
-          Through the power of AI, we seek to solve the problem of statically programmed scenarios in games.
-          Every decision the player makes can change the trajectory of the story.
+          PathFinder is a gaming web app that uses ChatGPT and Dall-E to create
+          dynamic story-telling experiences.
+          <br></br>
+          <br></br>
+          Through the power of AI, we seek to solve the problem of statically
+          programmed scenarios in games. Every decision the player makes can
+          change the trajectory of the story.
         </p>
       </div>
 
-      <div className="z-40">
-        <h2 className="text-3xl text-center tracking-wider mb-8">MEET THE TEAM</h2>
-        <div className="flex justify-center gap-12">
-
-          <div className="">
-            <div className="bg-blue-300 w-48 h-56 mb-2 berlin-avatar"></div>
-            <div className="text-center">
-              <p className="uppercase text-base">Berlin</p>
+      <div className="w-full z-10">
+        <h2 className="text-3xl text-center tracking-wider mb-8">
+          MEET THE TEAM
+        </h2>
+        <div className="grid grid-flow-col overflow-x-scroll px-5 gap-12">
+          {team_members.map((member, i) => (
+            <div className="flex flex-col items-center" key={i}>
+              <div
+                className={
+                  "team-member-banner border-4 rounded overflow-hidden  " +
+                  member.avatarClassName
+                }
+              >
+                <div className="team-member-name py-3 text-center w-full bg-gray-500 bg-opacity-50 backdrop-filter backdrop-blur-sm">
+                  <p className="uppercase text-base">{member.name}</p>
+                </div>
+              </div>
+              <section className="flex justify-evenly w-4/6 my-5">
+                <a className="small-gizmo bg-white rounded-full" href={member.github} target="_blank">
+                  <img src={github_icon} />
+                </a>
+                <a className="small-gizmo bg-white rounded-full" href={member.linkedin} target="_blank">
+                  <img src={linkedin_icon} />
+                </a>
+              </section>
             </div>
-          </div>
-
-          <div className="">
-            <div className="bg-blue-300 w-48 h-56 mb-2 frantz-avatar"></div>
-            <div className="text-center">
-              <p className="uppercase text-base">Frantz</p>
-            </div>
-          </div>
-
-          <div className="">
-            <div className="bg-blue-300 w-48 h-56 mb-2 wai-avatar"></div>
-            <div className="text-center">
-              <p className="uppercase text-base">Wai</p>
-            </div>
-          </div>
-
-          <div className="">
-            <div className="bg-blue-300 w-48 h-56 mb-2 sillis-avatar"></div>
-            <div className="text-center">
-              <p className="uppercase text-base">Sillis</p>
-            </div>
-          </div>
- 
+          ))}
         </div>
       </div>
-
       <div className="waves"></div>
     </div>
   );
